7b27d9f759c9f8cb25b0a1ef0c8d82cc12ec3f5c
Baker <kevin.baker@energytransfer.com>
2019-05-07 12:32:29 -0500

    Fixed some csproj issues

--- MicroTest.FileApi/Models/NtfsDirectory.cs
+++ MicroTest.FileApi/Models/NtfsDirectory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Security.AccessControl;
+using System.IO;
 
 namespace MicroTest.FileApi.Models
 {
--- MicroTest.FileApi/Startup.cs
+++ MicroTest.FileApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace MicroTest.FileApi
 {
@@ -24,8 +25,8 @@ namespace MicroTest.FileApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .AddNewtonsoftJson();
+            services.AddControllers();
+                //.AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
